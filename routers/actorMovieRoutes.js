7e0f6029138276e08303c30f192e570201e27f78
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-02-07 09:25:35 -0300

    deleting unnecessary folders

--- routers/actorMovieRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/actorsController');
-const router = express.Router();
-
-/* router.post('/new-movie-actor-relationship/', controller.createOneActor); */
